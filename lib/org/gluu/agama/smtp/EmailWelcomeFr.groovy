c3b4842244190c66490190e5ec20870d0598b5a5
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-10-23 20:32:03 +0530

    refactor: update EmailWelcomeFr.groovy

--- lib/org/gluu/agama/smtp/EmailWelcomeFr.groovy
+++ lib/org/gluu/agama/smtp/EmailWelcomeFr.groovy
@@ -31,7 +31,7 @@ class EmailWelcomeFr {
                         <p><strong>Détails de votre compte :</strong></p>
                         <div style="text-align:center;margin:30px 0;">
                           <div style="display:inline-block;background-color:#f5f5f5;color:#AD9269;font-size:28px;font-weight:600;letter-spacing:2px;padding:10px 20px;border-radius:4px;">
-                            """ + username + """
+                            """ + userName + """
                           </div>
                         </div>
 
