b5625b3d84387134198f2c781d6990b8d4701a60
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-10-23 21:15:43 +0530

    refactor: update NewUserRegistration.java

--- lib/org/gluu/agama/newuser/NewUserRegistration.java
+++ lib/org/gluu/agama/newuser/NewUserRegistration.java
@@ -26,7 +26,7 @@ public abstract class NewUserRegistration {
 
     public abstract boolean validateEmailOtp(String email, String emailOtp);
 
-    public abstract boolean sendRegSuccessEmail(String to, String userName, String lang);
+    public static  boolean sendRegSuccessEmail(String to, String userName, String lang);
 
     
     public static NewUserRegistration getInstance(HashMap config) {
