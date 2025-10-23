a38268ddf3bfa49288dc010e7245cbfae5464339
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-10-23 19:46:16 +0530

    refactor: update NewUserRegistration.java

--- lib/org/gluu/agama/newuser/NewUserRegistration.java
+++ lib/org/gluu/agama/newuser/NewUserRegistration.java
@@ -12,9 +12,7 @@ public abstract class NewUserRegistration {
 
     public abstract String addNewUser(Map<String, String> profile) throws Exception;
 
-    // public abstract boolean usernamePolicyMatch(String userName);
 
-    // public abstract boolean passwordPolicyMatch(String userPassword);
 
     public abstract String sendEmail(String to, String lang);
 
